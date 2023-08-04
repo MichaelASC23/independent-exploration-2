9cfe67215af46995da5ca1649dbe0411305376ad
MichaelASC23 <michaeljhleung@gmail.com>
2023-08-04 14:19:15 -0400

    final

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,11 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+// I'm lost 
+// counterElement.don't know what to do next
+function myVar() {
+counterElement.innerText = parseInt(counterElement.innerText) +1;
+console.log("test")
+}
+
+setInterval(myVar, 1000)
